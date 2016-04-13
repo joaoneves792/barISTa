2b815b70cc4b8378fd62907b2c8e3b8ea5ab6b66
Joao Neves <joaoneves792@gmail.com>
2016-04-13 10:47:53 +0100

    Changes in class

--- P2/jukebox.js
+++ P2/jukebox.js
@@ -4,11 +4,13 @@ function createTrendingTable(){
 	var table="<tr><td class=\"autor\"><b>Autor</b></td><td class=\"musica\"><b>Musica</b></td><td class=\"add\"></td></tr>";
 	var trending = [];
 	var list = playlist.slice(0);
-	for(i = 0; i<4; i++){
-		var random = Math.floor(Math.random()*list.length);
-		trending.push( list[random]);
-		list.splice(random, 1);
-	}
+	trending = list;
+
+	//for(i = 0; i<4; i++){
+	//	var random = Math.floor(Math.random()*list.length);
+	//	trending.push( list[random]);
+	//	list.splice(random, 1);
+	//}
 
 	//Create the table from the sorted list
 	for (i = 0; i <trending.length; i++) {
@@ -25,8 +27,8 @@ function searchMusic(){
 	var str = textInput.value;
 
 	for (i = 0; i<playlist.length; i++){
-		if(results.length >= 4)
-			break;
+		//if(results.length >= 4)
+		//	break;
 		var author = playlist[i].a.toUpperCase();
 		var song = playlist[i].s.toUpperCase();
 		if( (author.search(str.toUpperCase()) > -1) ||
